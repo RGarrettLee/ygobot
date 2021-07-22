8d4ccc385d7863b317dd687922f937e5a7341d5a
Garrett Lee <43485454+RGarrettLee@users.noreply.github.com>
2021-07-22 08:16:08 -0400

    Updates

--- cogs/cardList.py
+++ cogs/cardList.py
@@ -78,7 +78,7 @@ class CardList(commands.Cog):
                     self.currentSet = productData['data'][0]['card_sets'][i]['set_name']
             for i in range(len(productData['data'])):
                 for j in range(len(productData['data'][i]['card_sets'])):
-                    if (product == productData['data'][i]['card_sets'][i]['set_name'].lower()):
+                    if (product == productData['data'][i]['card_sets'][j]['set_name'].lower()):
                         setCards.append('{0}: **{1}**'.format(productData['data'][i]['name'], productData['data'][i]['card_sets'][0]['set_rarity']))
 
             out = ''
