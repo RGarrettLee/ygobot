f6331c3495694be50e5eeb1a76c28d31a6021e80
Garrett Lee <43485454+RGarrettLee@users.noreply.github.com>
2021-07-22 07:16:57 -0400

    Updated help

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ async def help(ctx):
     embed.add_field(name='{0}card (name)'.format(prefix), value='Brings up information about specified card', inline=False)
     embed.add_field(name='{0}staples'.format(prefix), value='Brings up current staples in the game', inline=False)
     embed.add_field(name='{0}banlist'.format(prefix), value='Brings up the current TCG banlist', inline=False)
+    embed.add_field(name='{0}.cardList/list (set name)'.format(prefix), value='Displays the cards in a specified set or structure/starter deck', inline=False)
     await ctx.send(embed=embed)
 
 for cog in os.listdir("./cogs"):
