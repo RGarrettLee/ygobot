4e1dd29aa373fd4f7848424df88474457bfbea31
Garrett Lee <43485454+RGarrettLee@users.noreply.github.com>
2021-12-05 17:14:30 -0500

    updates

--- main.py
+++ main.py
@@ -22,11 +22,11 @@ bot.remove_command('help')
 @bot.command()
 async def help(ctx):
     embed = discord.Embed(title='Help', description='All the functions this bot can do', color=0x0000ff)
-    embed.set_footer(text='Bot created by Sidewinder#4682')
     embed.add_field(name='{0}card (name)'.format(prefix), value='Brings up information about a specified card', inline=False)
     embed.add_field(name='{0}staples'.format(prefix), value='Brings up current staples in the game', inline=False)
     embed.add_field(name='{0}banlist'.format(prefix), value='Brings up the current TCG banlist', inline=False)
-    embed.add_field(name='{0}.cardList/list (set name)'.format(prefix), value='Displays the cards in a specified set or structure/starter deck', inline=False)
+    embed.add_field(name='{0}cardList/list (set name)'.format(prefix), value='Displays the cards in a specified set or structure/starter deck', inline=False)
+    embed.add_field(name='`Created By:`', value='<@!174263950685372417>', inline=False)
     await ctx.send(embed=embed)
 
 for cog in os.listdir("./cogs"):
